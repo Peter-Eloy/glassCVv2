84d572a1078fe63fc8ac2399f212774e70c6cedf
phoschka <phoschka@nartexsoft.com>
2025-01-31 11:36:53 +0000

    🚧

---
+++ src/components/mobileLandingPage/index.jsx
@@ -0,0 +1,14 @@
+// src/components/MobileLandingPage.jsx
+import React from "react";
+
+const MobileLandingPage = () => {
+  return (
+    <div style={{ textAlign: "center", padding: "20px" }}>
+      <h1>Welcome to glassCVv2</h1>
+      <p>This is the mobile landing page.</p>
+      {/* Add more content here */}
+    </div>
+  );
+};
+
+export default MobileLandingPage;
